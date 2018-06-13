556d9a05e9dfcb82157ac9837d36c98612e25752
wagnerperin <wagnerperin@gmail.com>
2018-06-13 17:47:50 -0300

    Modifying errorParser to render auth erros.

--- app/helpers/errorParser.js
+++ app/helpers/errorParser.js
@@ -12,6 +12,22 @@ module.exports = app => {
                     devMessage: 'Body does not contains an user object.'
                 };
                 break;
+            case 'auth-1':
+                error = {
+                    errorCode,
+                    moreInfo: app.get('errorsUri') + '/' + errorCode,
+                    userMessage: 'Erro na autenticação: Erro interno do servidor.',
+                    devMessage: errorObject
+                };
+                break;
+            case 'auth-2':
+                error = {
+                    errorCode,
+                    moreInfo: app.get('errorsUri') + '/' + errorCode,
+                    userMessage: 'Erro na autenticação: ' + errorObject.message,
+                    devMessage: errorObject
+                };
+                break;
         };
 
         return error;
